8f43dfdf5312a59b625f38394db05b733f674f97
Malaurynn <malauryn@icloud.com>
2025-01-08 16:35:38 +0100

    quest04

--- server/src/router.ts
+++ server/src/router.ts
@@ -8,22 +8,30 @@ const router = express.Router();
 
 // Define item-related routes
 import itemActions from "./modules/item/itemActions";
-import programActions from "./modules/program/programActions";
 
 router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
 
-router.get("/api/programs", programActions.browse);
+// Define program-related routes
+import programActions from "./modules/program/programActions";
 
+router.get("/api/programs", programActions.browse);
+router.get("/api/programs/:id", programActions.read);
+
+import categoryActions from "./modules/category/categoryActions";
+
+router.get("/api/categories", categoryActions.browse);
+router.get("/api/categories/:id", categoryActions.read);
 
 /* ************************************************************************* */
+
+// Declaration of a "Welcome" route
 
 import sayActions from "./modules/say/sayActions";
 
 router.get("/", sayActions.sayWelcome);
 
 /* ************************************************************************* */
-
 
 export default router;
