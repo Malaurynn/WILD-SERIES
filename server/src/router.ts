0dc7a54a8b329159d0e927fe26ded4a26191cee8
Malaurynn <malauryn@icloud.com>
2025-01-20 16:49:56 +0100

    bread and crud ok

--- server/src/router.ts
+++ server/src/router.ts
@@ -18,7 +18,7 @@ import programActions from "./modules/program/programActions";
 
 router.get("/api/programs", programActions.browse);
 router.get("/api/programs/:id", programActions.read);
-router.put("/api/programs:id", programActions.edit);
+router.put("/api/programs/:id", programActions.edit);
 router.post("/api/programs", programActions.add);
 router.delete("/api/programs/:id", programActions.destroy);
 
